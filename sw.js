af4ec8752e06aa9974f00aef6d3da1e8a0e2d34d
deborahalmeidatorres54-dot <deborahalmeidatorres54@gmail.com>
2026-02-23 10:48:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("paint-cache").then(cache => {
-      return cache.addAll([
-        "paint.html",
-        "manifest.json"
-      ]);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("paint-cache").then(cache =>
+      cache.addAll(["./", "./index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
